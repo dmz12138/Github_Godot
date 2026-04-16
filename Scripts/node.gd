5defb3c62d859f2d4e14be8e4796f6878349284e
unknown <2817454726@qq.com>
2026-04-17 00:33:52 +0800

    ysy

--- Scripts/node.gd
+++ Scripts/node.gd
@@ -1,13 +1,13 @@
-extends Node2D
+extends Node
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("我在网吧编辑了这行字符串")
-	print("ysy")
+	print("姚首次提交测试")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+Scenes/node.tscn
